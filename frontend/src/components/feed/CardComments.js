8caefbea73745fbb59664f2f71370fca5da55427
lauressl <laure.sasal@gmail.com>
2022-02-18 15:50:44 +0100

    fix delete comment

--- frontend/src/components/feed/CardComments.js
+++ frontend/src/components/feed/CardComments.js
@@ -1,22 +1,49 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { isEmpty } from '../utils';
+import { isEmpty, timestampParser } from '../utils';
+import deleteLogo from '../../images/trash.svg';
+
 
 const CardComments = ({post, postComment}) => {
     console.log(postComment)
-    const [text, settext] = useState("")
+    const ipServ=process.env.REACT_APP_IP_SERVER;
+
     const dispatch = useDispatch();
 
     const usersData = useSelector((state) => state.usersReducer);
     const userData = useSelector((state) => state.userReducer);
 
-    const handleComment = () => {};
+    const [isDeleted, setisDeleted] = useState(false)
+    //*****DELETE COMMENT*****/
+    const deleteComment = async () => {
+        let postId = post.id
+        try {
+            await axios.delete(ipServ + `/api/feed/post/comment/${postId}`,
+            { 
+                data: {
+                    postId : postId
+                },
+            
+                headers:{
+                    Authorization: `Bearer ${window.localStorage.getItem("token")}`
+                }
+            })
+            .then ((res) => {
+                console.log(res.data);
+                setisDeleted(true);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
 
   return (
-    <div className='comment-container'>
+    <div className="comments-container">
         {postComment.map((comment) => {
             return(
-            <div className={comment.userId === userData.id ? "comment-container-client" : "comment-container"} key={comment.id}>
+            <div className={isDeleted ? "comment-container deleted" : "comment-container"} key={comment.id}>
                 <div className='left-part'>
                     <img 
                         src={
@@ -29,8 +56,33 @@ const CardComments = ({post, postComment}) => {
                         alt="userpost-pic"/>
                 </div>
                 <div className='right-part'>
+                    <div className='comment-header'>
+                        <div className='pseudo'>
+                            <p>{!isEmpty(usersData[0]) &&
+                                usersData
+                                .map((user) => {
+                                    if(user.id === comment.userId) return user.username;
+                                }).join('')}
+                            </p>
+
+                        </div>
+                        <span>{timestampParser(comment.createdAt)}</span>       
+                    </div>
                     <p>{comment.content}</p>
                 </div>
+                {(userData.id === comment.userId || userData.isAdmin === true) &&
+                    <div className='btn-container'>
+                        <div onClick={(e) => {
+                            if(window.confirm("Voulez-vous supprimer cet article ?")){
+                                e.preventDefault(); 
+                                deleteComment(e);
+                            }
+                            }}
+                        >
+                            <img src={deleteLogo} alt="delete-icon"/>
+                        </div>
+                    </div>
+                }
             </div>
             )
         })
